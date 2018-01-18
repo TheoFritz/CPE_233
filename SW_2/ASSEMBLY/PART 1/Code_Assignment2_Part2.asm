274a3d3e1f21cfe91c1c5ee3b1b287dc20c1992f
Elizabeth Davis <edavis34@calpoly.edu>
2018-01-17 16:01:09 -0800

    Added the second part of software assignment 2

---
+++ SW_2/ASSEMBLY/PART 1/Code_Assignment2_Part2.asm
@@ -0,0 +1,37 @@
+.EQU IN_PORT = 0x30
+.EQU OUT_PORT = 0x42
+
+
+;--------------------------------------------------------------------------
+;-- .ORG used in code segment
+;--------------------------------------------------------------------------
+.CSEG
+.ORG 0x01 ; set the data segment counter to 0x01
+
+IN R0,IN_PORT
+MOV R1,R0
+
+CLC 
+LSL R1
+BRCS IS_ODD
+
+CLC
+LSL R1
+BRCC IS_DIVISIBLE_BY_FOUR
+
+SUB R0,0x01
+BRN OUTPUT
+
+IS_ODD:
+ADD R0,0x11
+CLC
+LSL R0
+BRN OUTPUT
+
+IS_DIVISIBLE_BY_FOUR:
+EXOR R0,0xFF
+BRN OUTPUT
+
+OUTPUT:
+OUT R0,OUT_PORT
+ 
