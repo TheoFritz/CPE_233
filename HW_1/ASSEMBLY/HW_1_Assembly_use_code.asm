5c20b53b06a789695dab5ddb4c50b9fbd2d039e9
Elizabeth Davis <edavis34@calpoly.edu>
2018-01-22 15:50:51 -0800

    Added comments to HW1 RAT Assignment

--- HW_1/ASSEMBLY/HW_1_Assembly_use_code.asm
+++ HW_1/ASSEMBLY/HW_1_Assembly_use_code.asm
@@ -1,4 +1,10 @@
-.EQU IN_PORT = 0x30
+; ----------------------------------------------------
+;
+; RAT ASSIGNMENT No. 1: PART 2A
+; DOMINIC GAIERO AND ELIZABETH DAVIS
+;
+; ----------------------------------------------------
+
 .EQU PWM_PORT = 0x40
 ;--------------------------------------------------------------------------
 ;-- .ORG used in data segment
@@ -13,13 +19,13 @@
 ;--------------------------------------------------------------------------
 .CSEG
 .ORG 0x01 ; set the data segment counter to 0x01
-MAIN : MOV R0,0x00  
-ADD_1: OUT R0,PWM_PORT
-       CMP R0,0xFF
-	   ADD R0,0x01
-       BRCS END_PROGRAM
-       BRN ADD_1
+MAIN : MOV R0,0x00  ; Moves 0x00 into R0
+ADD_1: OUT R0,PWM_PORT; Outputs the value of R0 to the PWM_port
+       CMP R0,0xFF ; Compares the value in R0 to 0xFF
+	   ADD R0,0x01 ; Adds one
+       BREQ END_PROGRAM ; Branches to END_PROGRAM if R0=255
+       BRN ADD_1 ; Loops the program by adding one each time until it reaches 255
 
-       END_PROGRAM:
+       END_PROGRAM: ; Terminates program
 
 
