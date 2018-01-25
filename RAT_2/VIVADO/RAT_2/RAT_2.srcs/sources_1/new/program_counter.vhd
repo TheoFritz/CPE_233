fbf74e51e5dbfc54a47b71dea563b5f75060319a
dgaiero <dpgaiero@gmail.com>
2018-01-24 23:20:21 -0800

    commented

--- RAT_2/VIVADO/RAT_2/RAT_2.srcs/sources_1/new/program_counter.vhd
+++ RAT_2/VIVADO/RAT_2/RAT_2.srcs/sources_1/new/program_counter.vhd
@@ -1,20 +1,17 @@
 ----------------------------------------------------------------------------------
--- Company:
--- Engineer:
 --
--- Create Date: 01/23/2018 10:46:34 PM
--- Design Name:
--- Module Name: program_counter - Behavioral
--- Project Name:
--- Target Devices:
--- Tool Versions:
--- Description:
+-- RAT ASSIGNMENT NO. 2:
+-- PROGRAM_COUNTER.VHD
+-- DOMINIC GAIERO AND ELIZABETH DAVIS
 --
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- The program counter (PC) takes in a 10 bit input (D_IN) from a mux.
+-- D_IN specifies where the PC starts counting.
+-- Data is only loaded into the PC when PC_ID is ‘1’.
+-- When PC_INC is a 1, the data is incremented, otherwise, the output is
+-- the same as the current value in the PC and is not incremented.
+-- RST resets the counter back to 0, and CLK is the timing signal that
+-- all operations operate on.
+-- PC_COUNT is a 10-bit output representing the current count.
 --
 ----------------------------------------------------------------------------------
 
@@ -26,30 +23,29 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity program_counter is
   Port (
-    D_IN: IN std_logic_vector(9 downto 0);
-    PC_LD: IN std_logic;
-    PC_INC: IN std_logic;
-    RST: IN std_logic;
-    CLK: IN std_logic;
-    PC_COUNT: OUT std_logic_vector(9 downto 0)
+    D_IN: IN std_logic_vector(9 downto 0); -- DATA IN
+    PC_LD: IN std_logic; -- PROGRAM COUNTER LOAD DATA
+    PC_INC: IN std_logic; -- PROGRAM COUNTER INCREMENT DATA
+    RST: IN std_logic; -- RESET
+    CLK: IN std_logic; -- CLOCK
+    PC_COUNT: OUT std_logic_vector(9 downto 0) -- PROGRAM COUNTER COUNT
   );
 end program_counter;
 
 architecture Behavioral of program_counter is
 
-  SIGNAL PC_COUNT_TMP : STD_LOGIC_VECTOR (9 DOWNTO 0);
+  SIGNAL PC_COUNT_TMP : STD_LOGIC_VECTOR (9 DOWNTO 0); -- TEMPORARY OUTPUT FOR INSIDE PROCESS STATEMENT
 
 
 begin
 
 
     pc: process(CLK)
-    -- variable CNT: integer := 0;
     begin
 
       IF (rising_edge(CLK)) THEN
-        IF (RST = '0') THEN
-          IF (PC_LD = '1') THEN
+        IF (RST = '0') THEN -- SYNCHRONOUS HIGH RESET
+          IF (PC_LD = '1') THEN -- PC_LD TAKES PRECEDANCE OVER PC_INT
             PC_COUNT_TMP <= D_IN;
           ELSIF (PC_LD = '0') THEN
             IF (PC_INC = '1') THEN
@@ -57,13 +53,13 @@ begin
             END IF;
           END IF;
         ELSE
-          PC_COUNT_TMP <= "0000000000";
+          PC_COUNT_TMP <= "0000000000"; -- SET PC_COUNT_TEMP TO 0x00
         END IF;
       END IF;
 
 
     end process;
 
-    PC_COUNT <= PC_COUNT_TMP;
+    PC_COUNT <= PC_COUNT_TMP; -- ELIMINATE STALE VALUES
 
 end Behavioral;
