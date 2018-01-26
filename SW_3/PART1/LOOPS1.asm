c3e49fa5722e265f9bc2a498084fd2b14d8b0338
dgaiero <dpgaiero@gmail.com>
2018-01-26 14:45:13 -0800

    Commented SW3

--- SW_3/PART1/LOOPS1.asm
+++ SW_3/PART1/LOOPS1.asm
@@ -4,7 +4,9 @@
 ; DOMINIC GAIERO AND ELIZABETH DAVIS
 ;
 ; DESCRIPTION:
-; Cycles through 0-255 and outputs it to PWM_PORT (0x40)
+; Take an input from port 0x9A (unsigned 8-bit). Split
+; input into two four-bits (top four and bottom four).
+; Multiply with numbers together and ouptut on 0x42.
 ; ----------------------------------------------------
 
 .EQU IN_PORT = 0x9A
@@ -18,21 +20,21 @@
 
 IN R0,IN_PORT
 MOV R1,R0
-MOV R2, 0x00
-MOV R3, 0x00
-AND R0,0x0F
-AND R1,0xF0
+MOV R2, 0x00 ; CLEAR TEMPORARY MULTIPLY REGISTER
+MOV R3, 0x00 ; CLEAR COUNT REGISTER
+AND R0,0x0F  ; MASK BOTTOM FOUR BITS
+AND R1,0xF0  ; MASK TOP FOUR BITS
 CLC
 LSR R1
 LSR R1
 LSR R1
-LSR R1
+LSR R1       ; SHIFT BITS DOWN TO BOTTOM FOUR
 
 MULTIPLY: 
-CMP R3,R1
-BREQ OUTPUT
+CMP R3,R1    ; CHECK IF TEMPORARY COUNT REG = THE MULTIPLIER VALUE
+BREQ OUTPUT  ; IF IT DOES, GO TO OUTPUT
 ADD R2, R0
-ADD R3, 0X01
+ADD R3, 0X01 ; INCREMENT COUNTER
 BRN MULTIPLY
 
 OUTPUT:
