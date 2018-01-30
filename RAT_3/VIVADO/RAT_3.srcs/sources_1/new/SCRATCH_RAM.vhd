de8e02f731546867e7df69e02fcbc093e1ec95f1
dgaiero <dpgaiero@gmail.com>
2018-01-29 16:24:10 -0800

    Block Diagrams and commented RAT_3

--- RAT_3/VIVADO/RAT_3.srcs/sources_1/new/SCRATCH_RAM.vhd
+++ RAT_3/VIVADO/RAT_3.srcs/sources_1/new/SCRATCH_RAM.vhd
@@ -1,63 +1,39 @@
 ----------------------------------------------------------------------------------
--- Company:
--- Engineer:
 --
--- Create Date: 01/28/2018 01:01:49 PM
--- Design Name:
--- Module Name: SCRATCH_RAM - Behavioral
--- Project Name:
--- Target Devices:
--- Tool Versions:
--- Description:
+-- RAT ASSIGNMENT NO. 3:
+-- SCRATCH_RAM.VHD
+-- DOMINIC GAIERO AND ELIZABETH DAVIS
 --
--- Dependencies:
+-- The Scratch RAM serves two functions. The first of those is to provide
+-- temporary storage that is accessible from the RAT instruction set. The second
+-- is that the RAT uses the scratch RAM as storage for the stack.
 --
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
 --
 ----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity SCRATCH_RAM is
-    Port ( DATA_IN : in STD_LOGIC_VECTOR (9 downto 0);
-           SCR_ADDR : in STD_LOGIC_VECTOR (7 downto 0);
-           SCR_WE : in STD_LOGIC;
-           CLK : in STD_LOGIC;
-           DATA_OUT : out STD_LOGIC_VECTOR (9 downto 0));
-end SCRATCH_RAM;
-
-architecture Behavioral of SCRATCH_RAM is
-
-    Type Vector_array is Array(0 to 255) of STD_LOGIC_VECTOR (9 DOWNTO 0);
-    signal memoryModule: Vector_array := (others => (others => '0'));
-
-
-
-begin
-
-  RAM: process(CLK)
-  begin
-    IF (rising_edge(CLK)) then
-      IF (SCR_WE = '1') then
-        memoryModule(to_integer(unsigned(SCR_ADDR))) <= DATA_IN;
-      END IF;
-    END IF;
-  end process;
-
-  DATA_OUT <= memoryModule(to_integer(unsigned(SCR_ADDR)));
-
-
-end Behavioral;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+ENTITY SCRATCH_RAM IS
+	PORT
+	(
+		DATA_IN  : IN STD_LOGIC_VECTOR (9 DOWNTO 0);
+		SCR_ADDR : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		SCR_WE   : IN STD_LOGIC;
+		CLK      : IN STD_LOGIC;
+		DATA_OUT : OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+	);
+END SCRATCH_RAM;
+ARCHITECTURE Behavioral OF SCRATCH_RAM IS
+	TYPE Vector_array IS ARRAY(0 TO 255) OF STD_LOGIC_VECTOR (9 DOWNTO 0);
+	SIGNAL memoryModule : Vector_array := (OTHERS => (OTHERS => '0')); -- Initalize to zero
+BEGIN
+	RAM : PROCESS (CLK)
+	BEGIN
+		IF (rising_edge(CLK)) THEN -- RAM writes synchronously
+			IF (SCR_WE = '1') THEN
+				memoryModule(to_integer(unsigned(SCR_ADDR))) <= DATA_IN;
+			END IF;
+		END IF;
+	END PROCESS;
+	DATA_OUT <= memoryModule(to_integer(unsigned(SCR_ADDR))); -- Data is read asynchronously
+END Behavioral;
