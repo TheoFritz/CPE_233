f61fcbcad01dee85daafdf57adca05e18b4e9eb9
dgaiero <dpgaiero@gmail.com>
2018-02-02 12:09:58 -0800

    More TB

--- SW_4/VIVADO/SW_4.srcs/sim_1/new/ALU_TB.vhd
+++ SW_4/VIVADO/SW_4.srcs/sim_1/new/ALU_TB.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use std.textio.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -35,7 +36,9 @@ entity ALU_TB is
 
 end ALU_TB;
 
-architecture Behavioral of ALU_TB is
+architecture stimulus of ALU_TB is
+
+FILE ALU_TB_VECS: TEXT OPEN READ_MODE IS "ALU_TB.TB";
 
 COMPONENT ALU IS
   Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
@@ -55,6 +58,8 @@ SIGNAL SUM_TB : STD_LOGIC_VECTOR (7 DOWNTO 0);
 SIGNAL C_FLAG_TB : STD_LOGIC;
 SIGNAL Z_FLAG_TB : STD_LOGIC;
 
+SIGNAL S_FAIL : STD_LOGIC := '0';
+
 
 begin
 
@@ -70,6 +75,60 @@ ALU_TB_UUT: ALU PORT MAP (
 
 );
 
+STIMULUS_PROCESS: PROCESS
+
+  VARIABLE V_VEC_LINE: LINE;
+  VARIABLE V_A, V_B : STD_LOGIC_VECTOR (7 DOWNTO 0);
+  VARIABLE V_SEL : STD_LOGIC_VECTOR (3 DOWNTO 0);
+  VARIABLE V_C_IN : STD_LOGIC;
+  VARIABLE V_SUM_TB : STD_LOGIC_VECTOR (7 DOWNTO 0);
+  VARIABLE V_C_FLAG, V_Z_FLAG : STD_LOGIC;
+
+BEGIN
+
+  WHILE NOT ENDFILE(ALU_TB_VECS) LOOP
+
+    READLINE(ALU_TB_VECS,V_VEC_LINE);
+
+    IF V_VEC_LINE(1) ='$' THEN
+      NEXT;
+    END IF;
+
+    READ(V_VEC_LINE,V_A);
+    READ(V_VEC_LINE,V_B);
+    READ(V_VEC_LINE,V_SEL);
+    READ(V_VEC_LINE,V_C_IN);
+
+    READ(V_VEC_LINE,V_SUM_TB);
+    READ(V_VEC_LINE,V_C_FLAG);
+    READ(V_VEC_LINE,V_Z_FLAG);
+
+    A_TB <= V_A;
+    B_TB <= V_B;
+    SEL_TB <= V_SEL;
+    C_IN_TB <= V_C_IN;
+    -- SUM_TB <= V_SUM_TB;
+    -- C_FLAG_TB <= V_C_FLAG;
+    -- Z_FLAG_TB <= V_Z_FLAG;
+
+    WAIT FOR 20 ns;
+
+     IF (SUM_TB /= V_SUM_TB) THEN
+      S_FAIL <= '1';
+    END IF;
+
+    ASSERT (SUM_TB = V_SUM_TB)
+      REPORT "SUM IS NOT CORRECT" SEVERITY ERROR;
+    ASSERT (C_FLAG_TB = V_C_FLAG)
+      REPORT "C_FLAG IS NOT CORRECT" SEVERITY ERROR;
+    ASSERT (Z_FLAG_TB = V_Z_FLAG)
+      REPORT "Z_FLAG IS NOT CORRECT" SEVERITY ERROR;
 
 
-end Behavioral;
+  END LOOP;
+  WAIT;
+END PROCESS;
+
+
+
+end stimulus;
