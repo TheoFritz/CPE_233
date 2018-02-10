1c1c50186487b90f45f1219e0f9273190d6f1432
dgaiero <dpgaiero@gmail.com>
2018-02-10 00:06:51 -0800

    Fixed signals

--- RAT_5/VIVADO/RAT_5.srcs/sources_1/new/CONTROL_UNIT.vhd
+++ RAT_5/VIVADO/RAT_5.srcs/sources_1/new/CONTROL_UNIT.vhd
@@ -92,6 +92,17 @@ COMB_PROC: PROCESS(OPCODE_HI_5, OPCODE_LO_2, sig_opcode_7, C_FLAG, Z_FLAG, PS, N
 BEGIN
 sig_OPCODE_7 <= OPCODE_HI_5 & OPCODE_LO_2;
 
+ I_SET        <= '0';    I_CLR        <= '0';
+PC_LD        <= '0';    PC_INC       <= '0';    PC_MUX_SEL   <= "00";
+ALU_OPY_SEL  <= '0';    ALU_SEL      <= "0000";
+RF_WR        <= '0';    RF_WR_SEL    <= "00";
+SP_LD        <= '0';    SP_INC       <= '0';    SP_DECR      <= '0';
+SCR_WE       <= '0';    SCR_ADDR_SEL <= "00";   SCR_DATA_SEL <= '0';
+FLG_C_SET    <= '0';    FLG_C_CLR    <= '0';    FLG_C_LD     <= '0';
+FLG_Z_LD     <= '0';    FLG_LD_SEL   <= '0';    FLG_SHAD_LD  <= '0';
+RST          <= '0';
+
+
 CASE PS IS
 WHEN  ST_INIT =>
   NS <= ST_FETCH;
@@ -117,31 +128,33 @@ WHEN ST_FETCH =>
   RST          <= '0';
 WHEN ST_EXEC =>
   NS <= ST_FETCH;
-  IF sig_OPCODE_7 <= "0010000" THEN -- BRN
+  PC_INC <= '0';
+  CASE SIG_OPCODE_7 IS
+  WHEN "0010000" => --BRN
     PC_LD <= '1';
-  ELSIF sig_OPCODE_7 <= "0000010" THEN -- EXOR (REG-REG)
+  WHEN "0000010" => -- EXOR (REG-REG)
     RF_WR <= '1';
     ALU_SEL <= "0111";
     FLG_C_CLR <= '1';
     FLG_Z_LD <= '1';
-  ELSIF OPCODE_HI_5 <= "10010" THEN -- EXOR (REG-IMMED)
+  WHEN "--"&"10010" => -- EXOR (REG-IMMED)
     RF_WR <= '1';
     ALU_SEL <= "0111";
     ALU_OPY_SEL <= '1';
     FLG_C_CLR <= '1';
     FLG_Z_LD <= '1';
-  ELSIF OPCODE_HI_5 <= "11001" THEN -- IN (REG-IMMED)
+  WHEN "11001"&"--" => -- IN (REG-IMMED)
     RF_WR <= '1';
     RF_WR_SEL <= "11";
-  ELSIF sig_OPCODE_7 <= "0001001" THEN -- MOV (REG-REG)
+  WHEN "0001001" => -- MOV (REG-REG)
     RF_WR <= '1';
     ALU_SEL <= "1110";
-  ELSIF OPCODE_HI_5 <= "11011" THEN -- MOV (REG-IMEMD)
+  WHEN "11011"&"--" => -- MOV (REG-IMEMD)
     RF_WR <= '1';
     ALU_SEL <= "1110";
     ALU_OPY_SEL <= '1';
-  ELSIF OPCODE_HI_5 <= "11010" THEN -- OUT (REG-REG)
-  ELSE
+--  ELSIF OPCODE_HI_5 <= "11010" THEN -- OUT (REG-REG)
+  WHEN OTHERS =>
     I_SET        <= '0';    I_CLR        <= '0';
     PC_LD        <= '0';    PC_INC       <= '0';    PC_MUX_SEL   <= "00";
     ALU_OPY_SEL  <= '0';    ALU_SEL      <= "0000";
@@ -151,7 +164,7 @@ WHEN ST_EXEC =>
     FLG_C_SET    <= '0';    FLG_C_CLR    <= '0';    FLG_C_LD     <= '0';
     FLG_Z_LD     <= '0';    FLG_LD_SEL   <= '0';    FLG_SHAD_LD  <= '0';
     RST          <= '0';
-  END IF;
+  END CASE;
 
 WHEN OTHERS =>
   I_SET        <= '0';    I_CLR        <= '0';
