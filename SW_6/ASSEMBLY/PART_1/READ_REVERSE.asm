284690da9afe3c31d98abcb59f1a3fe571b1a7bd
Elizabeth Davis <edavis34@calpoly.edu>
2018-02-16 15:38:27 -0800

    Added commented code

--- SW_6/ASSEMBLY/PART_1/READ_REVERSE.asm
+++ SW_6/ASSEMBLY/PART_1/READ_REVERSE.asm
@@ -3,21 +3,21 @@
 
 
 .CSEG
-.ORG 0x01
-MOV R0, 0x00
-READ_VALE_LOOP: IN R1, IN_PORT
-CMP R1, 0xFF
-BREQ POP_LOOP
-PUSH R1
-ADD R0, 0X01
-BRN READ_VALE_LOOP
+.ORG 0x01 ; Starts the cseg at 0x01
+MOV R0, 0x00 ; Moves 0x00 into R0 to acts as a counter
+READ_VALE_LOOP: IN R1, IN_PORT ; Takes a value from the in_port
+CMP R1, 0xFF ; Compares the value that was just inputted with 0xFF
+BREQ POP_LOOP; If the previous value that was inputted was 0xFF then the program jumps to the next loop
+PUSH R1 ; Otherwise the program pushes the inputted value to the stack
+ADD R0, 0X01 ; Adds 0x01 to the counter
+BRN READ_VALE_LOOP ; Branches to the start of the loop
 
 POP_LOOP:
-POP R1
-SUB R0, 0X01
-OUT R1, OUT_PORT
-CMP R0, 0X00
-BRNE POP_LOOP
-BREQ END
+POP R1 ; Pops the last value from the stack
+SUB R0, 0X01 ; Subtracts 1 from the counter
+OUT R1, OUT_PORT ; Outputs the popped value
+CMP R0, 0X00 ; Checks if the counter is 0x00
+BRNE POP_LOOP ; If the counter is not 0x00 the counter continues to pop values
+BREQ END ; If R0 is 0x00 it ends the program
 
 END:
--- SW_6/ASSEMBLY/PART_2/READ_FORWARD.asm
+++ SW_6/ASSEMBLY/PART_2/READ_FORWARD.asm
@@ -3,25 +3,25 @@
 
 
 .CSEG
-.ORG 0x01
+.ORG 0x01 ; Starts the cseg at 0x01
 
-MOV R0, 0x00
-MOV R3, 0x00
-READ_VALUE_LOOP: IN R1, IN_PORT
-CMP R1, 0xFF
-BREQ LOAD_LOOP
-PUSH R1
-ADD R0, 0X01
-BRN READ_VALUE_LOOP
+MOV R0, 0x00; Moves 0x00 into R0
+MOV R3, 0x00; Moves 0x00 into R3
+READ_VALUE_LOOP: IN R1, IN_PORT ; Takes a value in from the in_port
+CMP R1, 0xFF ; Compares the inputted value with 0xFF
+BREQ LOAD_LOOP ; If the inputted value is equal to 0xFF it will jump to the load loop
+PUSH R1 ; Pushes the inputted value to the stack
+ADD R0, 0X01 ; Adds one to the count register 
+BRN READ_VALUE_LOOP ; Branches to the READ_VALUE_LOOP
 
 LOAD_LOOP:
-LD R2,(R1)
-SUB R1, 0x01
-ADD R3, 0x01
-OUT R2, OUT_PORT
-CMP R3,R0
-BREQ END
-BRNE LOAD_LOOP
+LD R2,(R1) ; Loads the value in the location of R1 which is 0xFF
+SUB R1, 0x01 ; Subtracts one from R1
+ADD R3, 0x01 ; Adds one to R3
+OUT R2, OUT_PORT ; Outputs the value in R2 to the out_port
+CMP R3,R0 ; Compares the value in R3 with the value in the count register
+BREQ END ; If they are equal the program ends
+BRNE LOAD_LOOP ; If they are not equal the program continues to loop through LOAD_LOOP until they are
 
 
 END:
